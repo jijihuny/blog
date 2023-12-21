19c9c212d98dc8fe095e29768a52451e960ce507
JIJIHUNY <dlwlgns2792@gmail.com>
2023-12-22 05:49:43 +0900

    [feat] 반응형 크기, SearchBar

--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,24 @@
 import Link from 'next/link';
 import NavigationBar from './navigation-bar';
+import SearchBar from './search-bar';
 
-function Header() {
+type Props = {
+  menuList: string[]
+};
+
+function Header({ menuList }: Props) {
   return (
-    <header className="container flex flex-row justify-between items-center py-5 px-10 h-[5rem] bg-opacity-70 bg-white fixed [&+*]:pt-40 [&>*]:flex-grow">
-      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight">
+    <header className="container flex flex-row justify-between items-center py-5 px-10 h-[5rem] bg-opacity-70 bg-white fixed [&+*]:pt-40 z-50">
+      <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight flex-grow">
         <Link href="/" className="hover:underline">
           Blog
         </Link>
         .
       </h2>
-      <NavigationBar menuList={['Development', 'Life', 'Portfolio']} />
+      <span className="flex-grow-[3]">
+        <NavigationBar menuList={menuList} />
+      </span>
+      <SearchBar />
     </header>
   );
 }
