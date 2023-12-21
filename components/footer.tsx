3f07c7d28a5624f78157ccb5f6eef33095b0b0b3
JIJIHUNY <dlwlgns2792@gmail.com>
2023-12-22 05:46:13 +0900

    [chore] footer 반응형, 텍스트 수정

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,22 +5,23 @@ function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
+        <div className="p-12 lg:p-28 flex flex-col lg:flex-row justify-around items-center">
+          <h5 className="text-[1em] lg:text-[1.5em] font-medium tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            <p >COPYRIGHT 2023. 이지훈</p>
+            <p className='text-[0.8em]'>dlwlgns2792@gmail.com</p>
+          </h5>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
+              href="https://github.com/jijihuny"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Github Profile
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href={`https://github.com/jijihuny/blog`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              Repository
             </a>
           </div>
         </div>
